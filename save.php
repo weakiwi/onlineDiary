df3b97b8aa45b65406217e2dd334b42eff8d5c85
weakiwi <670932228@qq.com>
2016-04-24 17:27:16 +0800

    Update save.php

--- save.php
+++ save.php
@@ -1,19 +1,18 @@
 <?php
     header("content-Type: text/html; charset=gbk");
     error_reporting(E_ALL | E_STRICT);
-    $conn = mysql_connect("localhost", "root", "root");//创建sql连接
+    $mysqil = new mysqli("localhost", "root", "root", "myDiary");//创建sql连接
     if(! $conn )
     {
       die('Could not connect: ' . mysql_error());
     }    
-    mysql_select_db("myDiary");    
-    $createDiary = 'INSERT INTO diary (tomato,zhichu,sport,content) VALUES ("%s", "%u","%s","%s");';//增加用户
+    $createDiary = $mysql->prepare("INSERT INTO diary (tomato,zhichu,sport,content) VALUES (?, ?, ?, ?)");//增加用户
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $toamto = htmlspecialchars($_POST["tomato"]);
         $zhichu = htmlspecialchars($_POST["zhichu"]);
         $sport = htmlspecialchars($_POST["sport"]);
         $content = htmlspecialchars($_POST["content"]);
-        $createDiary = sprintf($createDiary, $toamto, $zhichu, $sport, $content);
-        mysql_query($createDiary);
+        $createDiary->bind_param($toamto, $zhichu, $sport, $content);
+        $conn->query($createDiary);
     }     
 ?>
